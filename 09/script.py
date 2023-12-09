4b52e47a78aa0c73eca2f7546f6a1f55b4d91554
Jonathan Balls <jonathanballs@protonmail.com>
2023-12-09 12:14:16 +0000

    09 update

--- 09/script.py
+++ 09/script.py
@@ -25,14 +25,10 @@ for line in integral_input:
 
 print("Part 1:", next_val_sum)
 
-def find_previous_integer(nums):
-    if is_all_zeroes(nums):
-        return 0
-    return nums[0] - find_previous_integer(differentiate(nums))
-
 prev_val_sum = 0
 for line in integral_input:
     vals = [int(n) for n in line.split()]
-    prev_val_sum += find_previous_integer(vals)
+    vals.reverse()
+    prev_val_sum += find_next_integer(vals)
 
 print("Part 2:", prev_val_sum)
